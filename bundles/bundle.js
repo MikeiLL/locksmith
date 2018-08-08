e5b6054d1233f984f45d1cf84d4e630430adcef7
Tom Carden <tom@lexwebdev.com>
2018-08-08 10:49:43 -0400

    about us and reviews tighten up.

--- bundles/bundle.js
+++ bundles/bundle.js
@@ -24,13 +24,11 @@ var velocity = require('velocity-animate');
 module.exports = function () {
     $('.section-2 .text')
         .velocity({
-            translateX: '-50%',
             scale: 0.4,
             opacity: 0
         }, 0)
         .delay(100)
         .velocity({
-            translateX: '-50%',
             scale: 1,
             opacity: 1
         }, 600, 'easeOutCubic');
