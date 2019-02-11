9358ac4ca7e26c76c34edac9bc5113c39e64ef01
Mike iLL Kilmer <mike@mzoo.org>
2019-02-11 14:45:31 -0600

    Add robots txt file.

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -avP robots.txt site.* *.ico *.svg *.png browserconfig.xml index.html bundles fonts images lock:public_html
+rsync -avP robots.txt sitemap.xml site.* *.ico *.svg *.png browserconfig.xml index.html bundles fonts images lock:public_html
